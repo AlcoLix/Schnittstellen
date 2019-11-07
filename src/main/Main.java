ed77a9406c77a77017f62d2fc53887684d7f16cc
Administrator <Administrator@10.98.163.18>
2019-11-07 18:18:20 +0100

    First Test of REST

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,32 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+        try {
+			URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP Error code : "
+                        + conn.getResponseCode());
+            }
+            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+            BufferedReader br = new BufferedReader(in);
+            String output;
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+            conn.disconnect();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
