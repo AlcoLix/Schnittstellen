1d97bb54f844b5417bb6f0692e085844fbdb79ca
Dennis <Administrator@10.98.163.18>
2019-11-08 15:57:17 +0100

    Implemented multiple url appenders

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,10 +7,15 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class Main {
-
+	private static String urlString;
 	public static void main(String[] args) {
 		try {
-			URL url = new URL("https://ghibliapi.herokuapp.com/films");
+			urlString = "https://calendarific.com/api/v2";
+			appendCountry("DE");
+			appendCredentials();
+			appendMethod("holidays");
+			appendYear(2019);
+			URL url = new URL(urlString);
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 			conn.setRequestMethod("GET");
 			conn.setRequestProperty("Accept", "application/json");
@@ -26,6 +31,40 @@ public class Main {
 			conn.disconnect();
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+	}
+	private static void appendMethod(String method) {
+		
+		if(urlString.contains("?")) {
+			int pos = urlString.indexOf('?');
+			urlString = urlString.substring(0, pos)+"/"+method+urlString.substring(pos);
+			//Alternativ:
+//			String[] parts = urlString.split("\\?");
+//			urlString += parts[0]+"/"+method+"?"+parts[1];
+		}else {
+			urlString += "/"+method;
+		}
+	}
+	private static void appendCountry(String country) {
+		checkAndAppendConcatenator();
+		urlString += "country="+country;
+	}
+	private static void appendYear(int year) {
+		checkAndAppendConcatenator();
+		urlString += "year="+year;
+	}
+	private static void appendYear(String year) {
+		appendYear(Integer.parseInt(year));
+	}
+	private static void appendCredentials() {
+		checkAndAppendConcatenator();
+		urlString += "api_key=416952d00c2786b01c36f84da35bd28937656220"; 
+	}
+	private static void checkAndAppendConcatenator() {
+		if(urlString.contains("?")) {
+			urlString += "&";
+		}else {
+			urlString += "?";
 		}
 	}
 }
