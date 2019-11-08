e27a474725b1182b422e69a32d7ac76afd2bf9ce
Administrator <Administrator@10.98.163.18>
2019-11-08 14:41:13 +0100

    Changed URL

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,22 +9,21 @@ import java.net.URL;
 public class Main {
 
 	public static void main(String[] args) {
-        try {
-			URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily");
+		try {
+			URL url = new URL("https://ghibliapi.herokuapp.com/films");
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Accept", "application/json");
-            if (conn.getResponseCode() != 200) {
-                throw new RuntimeException("Failed : HTTP Error code : "
-                        + conn.getResponseCode());
-            }
-            InputStreamReader in = new InputStreamReader(conn.getInputStream());
-            BufferedReader br = new BufferedReader(in);
-            String output;
-            while ((output = br.readLine()) != null) {
-                System.out.println(output);
-            }
-            conn.disconnect();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP Error code : " + conn.getResponseCode());
+			}
+			InputStreamReader in = new InputStreamReader(conn.getInputStream());
+			BufferedReader br = new BufferedReader(in);
+			String output;
+			while ((output = br.readLine()) != null) {
+				System.out.println(output);
+			}
+			conn.disconnect();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
