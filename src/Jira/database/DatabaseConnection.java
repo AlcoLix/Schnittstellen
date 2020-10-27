a941d2ea5f452f62feabbfd619fe273f9ebb9cbf
Dennis Rünzler <dennis@ruenzler.de>
2020-10-27 13:33:38 +0100

    bugfix

--- src/Jira/database/DatabaseConnection.java
+++ src/Jira/database/DatabaseConnection.java
@@ -56,7 +56,7 @@ public class DatabaseConnection {
 				
 				String UP_SerNo;
 				UP_SerNo = cstmt.getString(3);
-				if(UP_SerNo.equalsIgnoreCase("null")) {
+				if(UP_SerNo == null || UP_SerNo.equalsIgnoreCase("null")) {
 					UP_SerNo = "";
 				}
 //				System.out.println("result:"+UP_SerNo);
